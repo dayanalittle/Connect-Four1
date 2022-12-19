bae626048106a47c7b47f08f1a8355aed7b8d31e
Day Little <dayana.y.little@gmail.com>
2022-12-19 10:24:34 -0500

    define varibles

--- js/app.js
+++ js/app.js
@@ -1,6 +1,17 @@
 
 
 
+// Create Board  6 by 7 
+// Players Chooses preferred color 
+// Needs a alternating  turn function 
+// alert function if player pieces reach the top of board 
+// Alert is spot is already taken 
+// Check for winner function 
+// Needs winning combinations list
+// check vertically horizontally and diagonality for winner 
+// Drop the chips at end of game (for winner)
+// Algorithm that accounts  for winner similar to TTT
+// Reset game functiin
 
 
 
@@ -10,14 +21,21 @@
 
 /*-------------------------------- Constants --------------------------------*/
 
-
+const winningCombos = [
+    [0,1,2,3]
+ 
+ ]
+ 
 
 
 
 
 /*---------------------------- Variables (state) ----------------------------*/
 
-
+let board 
+let turn 
+let winner 
+let tie
  
 
 
